4be48d14c6eea5c27bf94a9d1b39ad56e7f8fde1
Joseph Venetucci <venetuc@pdx.edu>
2018-04-22 19:50:45 -0700

    updated sawtooth python sdk to 1.0.2

--- processor/env/bin/activate.fish
+++ processor/env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/joseph/pizzaDemo/Processor/env"
+set -gx VIRTUAL_ENV "/Users/joseph/dockerTest/lace/processor/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
