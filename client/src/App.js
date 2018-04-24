1d384ee6e75b4cf90876a51cfe286f54b217a4f6
NickNation <nnation@pdx.edu>
2018-04-24 13:33:27 -0700

    The code to display the table.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './styles/App.css';
 
 import Form from './Form';
-import DisplayInventory from './DisplayInventory';
+import DisplayInventory from './DisplayInventory.js';
 
 class App extends Component {
   render() {
