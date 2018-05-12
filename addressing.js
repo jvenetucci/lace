fe143eeceef91d831ba99c9613cfa7897cec155b
Roberto <ravila@pdx.edu>
2018-05-12 15:42:02 -0700

    Added javascript addressing file.

---
+++ addressing.js
@@ -0,0 +1,42 @@
+var crypto = require('crypto');
+
+const FAMILY_NAME = 'lace';
+const NAMESPACE = hash(FAMILY_NAME).slice(0, 6);
+const ASSET = '0';
+const HISTORY = '1';
+const AGENT = '2';
+
+function hash(string) {
+    return crypto.createHash('sha512').update(string).digest("hex");
+}
+
+function makeTouchpointPostfix(num) {
+    return num.toString(16).padStart(4, "0");
+}
+
+function makeAssetAddress(identifier) {
+    return  NAMESPACE + 
+            ASSET + 
+            hash(identifier).slice(0, 59) + 
+            '0000';
+}
+
+function makeHistoryAddress(identifier) {
+    return  NAMESPACE + 
+            HISTORY + 
+            hash(identifier).slice(0, 59) + 
+            '0000';
+}
+
+function makeTouchpointAddress(identifier, index) {
+    return  NAMESPACE + 
+            HISTORY + 
+            hash(identifier).slice(0, 59) + 
+            makeTouchpointPostfix(index);
+}
+
+function makeAgentAddress(identifier) {
+    return  NAMESPACE + 
+            AGENT + 
+            hash(identifier).slice(0, 63);
+}
