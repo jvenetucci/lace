67a38931d62f1ddcfd6a5dfda44ba912e0d5f4a6
NickNation <nnation@pdx.edu>
2018-05-23 00:29:54 -0700

    Asset history will be displayed accurately in OwnerClient. Formatting is gross right now, but all things in good time.

--- ClientApps/OwnerClient/src/pages/AssetHistoryPage.js
+++ ClientApps/OwnerClient/src/pages/AssetHistoryPage.js
@@ -11,6 +11,52 @@ class AssetHistoryPage extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
   }
+
+  addTableHead(rfidTag) {
+    var tableRef = document.getElementById('histTable');
+    tableRef.deleteTHead();
+    tableRef.deleteCaption();
+    
+    var tableCaption = tableRef.createCaption();
+    tableCaption.align = 'left';
+    var captionText = document.createTextNode('History of ' + rfidTag);
+    tableCaption.appendChild(captionText);
+  
+  
+    var tableHead = tableRef.createTHead();
+    var newHeadRow = tableHead.insertRow(-1);
+    var newHeadCell = newHeadRow.insertCell(-1);
+    var cellText = document.createTextNode("Holder");
+    newHeadCell.appendChild(cellText);
+  }
+  
+  addTableRows(itemHistory) {
+    var tableRef = document.getElementById('histTable');
+  
+    itemHistory.forEach(element => {
+    var newRow = tableRef.insertRow(-1);
+    var newCell = newRow.insertCell(-1);
+
+    var cellText = document.createTextNode(this.resolvePubKeyToName(element.publicKey));
+    newCell.appendChild(cellText);
+    });
+  
+  }
+
+  resolvePubKeyToName(keyToResolve) {
+
+    if('026d529e3955f7cde5f89dec9dc1defeeffe462765a183c411cafa32406eb2a990' === keyToResolve) {
+      return 'ShipperBoat';
+    } else if('031498a3d386e2ff9c6e6eca56f48f8d796248aa3135c9f88e5d7f871bfa12a7ca' === keyToResolve) {
+      return 'Shipper';
+    } else if('020b0132a725e8fe6a6ee74a902cfc3f0bcbb7ae7b5d2218aad18b16df422a0f5d' === keyToResolve) {
+      return 'Company';
+    } else if('02bc83bb8d3f5e99ab4cde7bb576ecb6898683261cc044349c15b7076261555f68' === keyToResolve) {
+      return 'Factory';
+    } else {
+      return 'ShipperTruck2';
+    }
+  }
   
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
@@ -40,12 +86,14 @@ class AssetHistoryPage extends Component {
       let jsonObj = JSON.parse(obj);
 
       //Reference for names of JSON fields.
-      //alert(obj);
+      alert(obj);
 
       //Extract relevant info.
       var rfid = jsonObj.entriesList[0].rfid;
       var reporterList = jsonObj.entriesList[0].reporterListList;
       alert('rfid: ' + rfid + '\nreporter list:' + reporterList);
+      this.addTableHead(rfid);
+      this.addTableRows(reporterList);
     }))
   });
     event.preventDefault();
@@ -58,7 +106,7 @@ class AssetHistoryPage extends Component {
           <form onSubmit={this.handleSubmit}>
             <legend>Display History Form</legend>
             <div className="form-group">
-              <label>RFRID</label>
+              <label>RFID</label>
               <input 
                 type="text" 
                 name="rfid"
